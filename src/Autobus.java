3168ebf08ee2946d88c996218dbba897c9851aa6
saulfabre <saulfabre2@gmail.com>
2026-05-18 09:17:44 -0400

    Corregir metodo mostrarDatos en ejercicio 8

--- src/Autobus.java
+++ src/Autobus.java
@@ -10,10 +10,12 @@ public class Autobus extends Vehiculos {
 
     public void mostrarDatos() {
 
+        System.out.println("--------------------------------");
+        System.out.println("Autobutos hereda de vehiculo: ");
         super.mostrarDatos();
+        System.out.println("--------------------------------");
+        System.out.println("Atributo del autobus: ");
         System.out.println(">> Numero de pasajeros: " + numeroPasajeros);
         System.out.println("--------------------------------");
-    }
-
-    
+    }  
 }
--- src/Carro.java
+++ src/Carro.java
@@ -10,10 +10,12 @@ public class Carro extends Vehiculos {
 
     public void mostrarDatos() {
 
+        System.out.println("--------------------------------");
+        System.out.println(">> Carro hereda de vehiculo: ");
         super.mostrarDatos();
+        System.out.println("--------------------------------");
+        System.out.println("Atributo del carro: ");
         System.out.println(">> Cantidad de puertas: " + cantidadPuertas);
         System.out.println("--------------------------------");
     }
-
-
 }
